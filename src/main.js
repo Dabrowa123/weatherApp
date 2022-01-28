a462e99e335579c9b7e1b0326404734ed6a1e102
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-01-28 11:03:04 +0000

    connecting DOM with js, creating initial functions in main

---
+++ src/main.js
@@ -0,0 +1,36 @@
+
+const viewElems = {};
+
+const getElemDOM = id => {
+    return document.getElementById(id);
+}
+
+const connectHTMLElemens = () => {
+    viewElems.weatherSearchView = getElemDOM("weatherSearchView");
+    viewElems.mainContainer = getElemDOM("mainContainer");
+    viewElems.weatherForecastView = getElemDOM("weatherForecastView");
+    
+    viewElems.searchInput = getElemDOM("searchInput");
+    viewElems.searchButton = getElemDOM("searchButton");
+    viewElems.weatherCity = getElemDOM("weatherCity");
+    viewElems.weatherIcon = getElemDOM("weatherIcon");
+    viewElems.weatherCurrentTemp = getElemDOM("weatherCurrentTemp");
+    viewElems.weatherMaxTemp = getElemDOM("weatherMaxTemp");
+    viewElems.weatherMinTemp = getElemDOM("weatherMinTemp");
+}
+
+
+const setupListeners = () => {
+    viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
+    viewElems.searchButton.addEventListener('click', onClickSubmit);
+}
+
+const initializeApp = () => {
+    connectHTMLElemens();
+    setupListeners();
+}
+
+const onClickSubmit = () => {};
+const onEnterSubmit = () => {};
+
+document.addEventListener('DOMContentLoaded', initializeApp);

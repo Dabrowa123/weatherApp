654dc6ff76fd43697231352f29ed0acbde5a2dc4
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-02-14 10:27:35 +0000

    fadeInOut function

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const connectHTMLElemens = () => {
     
     viewElems.searchInput = getElemDOM("searchInput");
     viewElems.searchButton = getElemDOM("searchButton");
+    viewElems.returnToSearchBtn = getElemDOM("returnToSearchBtn");
     viewElems.weatherCity = getElemDOM("weatherCity");
     viewElems.weatherIcon = getElemDOM("weatherIcon");
     viewElems.weatherCurrentTemp = getElemDOM("weatherCurrentTemp");
@@ -25,6 +26,7 @@ const connectHTMLElemens = () => {
 const setupListeners = () => {
     viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
     viewElems.searchButton.addEventListener('click', onClickSubmit);
+    viewElems.returnToSearchBtn.addEventListener('click', returnToSearch);
 }
 
 const initializeApp = () => {
@@ -32,13 +34,51 @@ const initializeApp = () => {
     setupListeners();
 }
 
-const onClickSubmit = () => {};
+const onClickSubmit = () => {
+    returnToSearch();
+    let query = viewElems.searchInput.value;
+    getWeatherByCity(query).then(data => 
+        console.log(data));
+    // fadeInOut();
+    // switchView();
+};
+
 const onEnterSubmit = event => {
     if (event.key === 'Enter') {
+        returnToSearch();
         let query = viewElems.searchInput.value;
-        getWeatherByCity(query).then(data =>
-            console.log(data));
+        getWeatherByCity(query).then(data => {
+            console.log(data);
+        });
+        // switchView();
+        // fadeInOut();
     }
 };
+
+const fadeInOut = () => {
+    if (viewElems.mainContainer.style.opacity === '1' || viewElems.mainContainer.style.opacity === '') {
+        viewElems.mainContainer.style.opacity = '0';
+    } else {
+        viewElems.mainContainer.style.opacity = '1';
+    }
+}
+
+const switchView = () => {
+    if (viewElems.weatherSearchView.style.display !== 'none') {
+        viewElems.weatherSearchView.style.display = 'none';
+        viewElems.weatherForecastView.style.display = 'flex';
+    } else {
+        viewElems.weatherSearchView.style.display = 'flex';
+        viewElems.weatherForecastView.style.display = 'none';
+    }
+}
+
+const returnToSearch = () => {
+    fadeInOut();
+    setTimeout( () => {
+        switchView();
+        fadeInOut();
+    }, 500);
+}
 
 document.addEventListener('DOMContentLoaded', initializeApp);

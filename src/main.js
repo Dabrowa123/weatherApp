a94d09f825aae553c50a68d629eb92608db83f1b
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-02-15 16:19:56 +0000

    finish of v1.1

--- src/main.js
+++ src/main.js
@@ -37,10 +37,14 @@ const initializeApp = () => {
 const onClickSubmit = () => {
     returnToSearch();
     let query = viewElems.searchInput.value;
-    getWeatherByCity(query).then(data => 
-        console.log(data));
-    // fadeInOut();
-    // switchView();
+    console.log("hello");
+    getWeatherByCity(query).then(data => {
+        console.log("hello1");
+        console.log(data);
+        displayWeatherData(data);
+        // fadeInOut();
+        // switchView();
+    });
 };
 
 const onEnterSubmit = event => {
@@ -48,12 +52,29 @@ const onEnterSubmit = event => {
         returnToSearch();
         let query = viewElems.searchInput.value;
         getWeatherByCity(query).then(data => {
+            console.log("hello1");
+            displayWeatherData(data);
             console.log(data);
         });
         // switchView();
         // fadeInOut();
     }
 };
+
+const displayWeatherData = data => {
+    const weather = data.consolidated_weather[0];
+
+    viewElems.weatherCity.innerText = data.title;
+    viewElems.weatherIcon.src = `https://www.metaweather.com/static/img/weather/png/64/${weather.weather_state_abbr}.png`;
+    viewElems.weatherIcon.alt = weather.weather_state_name;
+    const currentTemp = weather.the_temp.toFixed(2);
+    const maxTemp = weather.max_temp.toFixed(2);
+    const minTemp = weather.min_temp.toFixed(2);
+
+    viewElems.weatherCurrentTemp.innerText = `Current temperature is: ${currentTemp}°C`;
+    viewElems.weatherMaxTemp.innerText = `Max temperature is: ${maxTemp}°C`;
+    viewElems.weatherMinTemp.innerText = `Min temperature is: ${minTemp}°C`;
+}
 
 const fadeInOut = () => {
     if (viewElems.mainContainer.style.opacity === '1' || viewElems.mainContainer.style.opacity === '') {
@@ -66,7 +87,7 @@ const fadeInOut = () => {
 const switchView = () => {
     if (viewElems.weatherSearchView.style.display !== 'none') {
         viewElems.weatherSearchView.style.display = 'none';
-        viewElems.weatherForecastView.style.display = 'flex';
+        viewElems.weatherForecastView.style.display = 'block';
     } else {
         viewElems.weatherSearchView.style.display = 'flex';
         viewElems.weatherForecastView.style.display = 'none';
